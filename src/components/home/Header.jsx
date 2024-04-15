38124a78c9ff715e604990d34df89cdb1f7deadb
Muhammad Abbas <md.devjs@gmail.com>
2024-04-15 21:00:16 +0200

    adjust bg home opacity

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -3,7 +3,7 @@ import Nav from '../navigation/Nav';
 
 function Header() {
   return (
-    <header className="min-h-[100dvh] bg-cover bg-center bg-green bg-home bg-repeat-none">
+    <header className="min-h-[100dvh] bg-cover bg-center bg-green/20 bg-home bg-repeat-none">
       <Nav />
     </header>
   );
