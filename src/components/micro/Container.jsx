28f92c7e1fc2eedb2851876ff5bc68f3b445b2ea
Muhammad Abbas <md.devjs@gmail.com>
2024-04-17 06:52:21 +0200

    adjust container padding for mobile screens

--- src/components/micro/Container.jsx
+++ src/components/micro/Container.jsx
@@ -1,6 +1,6 @@
 'use client';
 function Container({ children }) {
-  return <div className="container p-8 mx-auto md:p-10 lg:px-12 xl:px-14 2xl:px-16">{children}</div>;
+  return <div className="container p-6 mx-auto sm:p-8 md:p-10 lg:px-12 xl:px-14 2xl:px-16">{children}</div>;
 }
 
 export default Container;
