f0936f5049fdb2348af0feaa6cd36c4acf9173ec
Muhammad Abbas <md.devjs@gmail.com>
2024-04-17 07:27:05 +0200

    add extra 2 intro video sizes

--- src/components/home/IntroVideo.jsx
+++ src/components/home/IntroVideo.jsx
@@ -2,8 +2,10 @@
 function IntroVideo() {
   return (
     <video autoPlay loop muted playsInline className="w-full">
+      <source src="dots-intro-423.mp4" type="video/mp4" media="(max-width:423px)" />
       <source src="dots-intro-640.mp4" type="video/mp4" media="(max-width:640px)" />
-      <source src="dots-intro-1280.mp4" type="video/mp4" media="(max-width:1280px)" />
+      <source src="dots-intro-853.mp4" type="video/mp4" media="(max-width:853px)" />
+      <source src="dots-intro-1280.mp4" type="video/mp4" media="(max-width:1536px)" />
       <source src="dots-intro-1920.mp4" type="video/mp4" />
       <p className="text-center">Your browser does not support the video tag. 😞</p>
     </video>
