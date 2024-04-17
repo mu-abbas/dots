546010984c03a697da6c360d8d97c90a70b90cd7
Muhammad Abbas <md.devjs@gmail.com>
2024-04-17 06:35:03 +0200

    compress intro video and add multiple sources

--- src/components/home/IntroVideo.jsx
+++ src/components/home/IntroVideo.jsx
@@ -1,8 +1,10 @@
 'use client';
 function IntroVideo() {
   return (
-    <video autoPlay loop muted className="w-full">
-      <source src="dots-intro.mp4" type="video/mp4" />
+    <video autoPlay loop muted playsInline className="w-full">
+      <source src="dots-intro-640.mp4" type="video/mp4" media="(max-width:640px)" />
+      <source src="dots-intro-1280.mp4" type="video/mp4" media="(max-width:1280px)" />
+      <source src="dots-intro-1920.mp4" type="video/mp4" />
       <p className="text-center">Your browser does not support the video tag. 😞</p>
     </video>
   );
