c68a67ee4fa83caa1558ee635eee04fbed0ea78c
Muhammad Abbas <md.devjs@gmail.com>
2024-04-19 06:13:35 +0200

    fix featured hover state for mobile screens

--- src/components/home/FeaturedImage.jsx
+++ src/components/home/FeaturedImage.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 function FeaturedImage({ src, alt, href, description }) {
   return (
     <Link className="relative overflow-hidden rounded-xl group" href={href}>
-      <img src={src} alt={alt} />
-      <span className="absolute inset-0 flex items-center justify-center backdrop-blur-[2px] opacity-0 group-hover:opacity-100 transition duration-300 backdrop-brightness-50 text-white flex-col gap-4">
+      <img src={src} alt={alt} className="group-hover:brightness-50 group-hover:blur-[2px]" />
+      <span className="absolute inset-0 flex flex-col items-center justify-center gap-4 text-white transition duration-300 opacity-0 group-hover:opacity-100">
         <span className="text-4xl lg:text-5xl">{alt}</span>
         {description && <span className="font-light">{description}</span>}
       </span>
