c662ad258b18f7aa14ce4ed80c33b0b69cc842ea
Muhammad Abbas <md.devjs@gmail.com>
2024-04-20 06:09:42 +0200

    working on testimonials

--- src/components/home/TestimoialCard.jsx
+++ src/components/home/TestimoialCard.jsx
@@ -1,13 +1,13 @@
 'use client';
 function TestimoialCard({ quote, image, name, role }) {
   return (
-    <figure>
-      <blockquote>{quote}</blockquote>
-      <figcaption>
-        <img src={image} alt={name} />
-        <div>
-          <cite className="capitalize">{name}</cite>
-          <span>{role}</span>
+    <figure className="flex flex-col justify-between py-8 h-72">
+      <blockquote className="max-w-sm px-2 text-lg font-light leading-7 text-grey font-poppins">{quote}</blockquote>
+      <figcaption className="flex items-center gap-6">
+        <img src={image} alt={name} className="rounded-full size-24" />
+        <div className="flex flex-col">
+          <cite className="font-bold capitalize">{name}</cite>
+          <span className="text-sm font-thin opacity-75 text-grey">{role}</span>
         </div>
       </figcaption>
     </figure>
--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -5,6 +5,7 @@ import SpaceY from '../micro/SpaceY';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import TestimoialCard from './TestimoialCard';
 
 let nextOnClick;
 
@@ -17,7 +18,7 @@ function NextArrow(props) {
 
   return (
     <div
-      className={`${className} hideMeOnTouchScreens`}
+      className={className}
       style={{
         ...style,
         bottom: '0',
@@ -36,7 +37,7 @@ function PrevArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} hideMeOnTouchScreens`}
+      className={className}
       style={{
         ...style,
         bottom: '0',
@@ -51,7 +52,50 @@ function PrevArrow(props) {
   );
 }
 
-const testimonials = [{}];
+const testimonials = [
+  {
+    quote:
+      '“Outstanding service, great value. They understood and met our needs on time. Highly recommended for future projects.”',
+    image: 'image/home/Rick-Mare.jpg',
+    name: 'Rick Mare',
+    role: 'CEO',
+  },
+  {
+    quote:
+      '“They consistently provide high-quality designs promptly and are great communicators. Having worked together four times.”',
+    image: 'image/home/Lynda.jpg',
+    name: 'Lynda Ford',
+    role: 'CEO',
+  },
+  {
+    quote:
+      '“They precisely met the brief with perfect, timely execution in Pitch Decks. Highly talented, will collaborate again.”',
+    image: 'image/home/Jack.jpg',
+    name: "Jack O'Holleran",
+    role: 'CEO',
+  },
+  {
+    quote:
+      '“Exceptional work - professional, ethical, and impressive. Highly satisfied and discussing future projects already!”',
+    image: 'image/home/Benjamin-Mill.jpg',
+    name: 'Benjamin Mill',
+    role: 'Managing Director',
+  },
+  {
+    quote:
+      '“They excelled in creating a Google Slides template, seamlessly incorporating feedback, proving to be valuable partners.”',
+    image: 'image/home/Sven-Montanus.jpg',
+    name: 'Sven Montanus',
+    role: 'Managing Partner',
+  },
+  {
+    quote:
+      '“They consistently delivered exceptional work on multiple projects, prioritizing quality, timely completion, and focus..”',
+    image: 'image/home/Don-Philabaum.png',
+    name: 'Don Philabaum',
+    role: 'Author',
+  },
+];
 
 function Testimonials() {
   const [activeSlide, setActiveSlide] = useState(0);
@@ -77,9 +121,9 @@ function Testimonials() {
   }, []);
 
   useEffect(() => {
-    if (windowWidth < 1024) setSlides(2);
-    if (windowWidth < 700) setSlides(1);
-    if (windowWidth > 1024) setSlides(3);
+    if (windowWidth < 1024) setSlides(1);
+    if (windowWidth < 1536 && windowWidth > 1024) setSlides(2);
+    if (windowWidth > 1536) setSlides(3);
   }, [windowWidth]);
 
   const settings = {
@@ -103,30 +147,15 @@ function Testimonials() {
             <h2 className="px-4 py-3 text-xl font-medium text-center border rounded-lg md:px-6 md:py-4 opacity-85 text-grey border-grey min-w-max sm:text-2xl lg:text-3xl 3xl:text-4xl">
               What our clients say
             </h2>
-            <p className="max-w-sm font-light md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-poppins sm:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-3xl 3xl:max-w-4xl 3xl:text-4xl">
+            <p className="max-w-sm font-light leading-6 md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-poppins sm:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-3xl 3xl:max-w-4xl 3xl:text-4xl">
               Proudly serving a diverse range of clients,from startups to Fortune 500 companies. They trusted us! …..
             </p>
           </div>
           <div className="relative" onClick={nextOnClick}>
             <Slider {...settings}>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">1</h3>
-              </div>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">2</h3>
-              </div>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">3</h3>
-              </div>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">4</h3>
-              </div>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">5</h3>
-              </div>
-              <div>
-                <h3 className="p-20 mx-4 bg-blue">6</h3>
-              </div>
+              {testimonials.map(({ quote, image, name, role }) => (
+                <TestimoialCard role={role} image={image} quote={quote} name={name} key={name} />
+              ))}
             </Slider>
             <div className="relative h-1 overflow-hidden" role="progressbar">
               <span className="absolute left-0 w-full h-1 opacity-25 bg-grey"></span>
@@ -136,6 +165,7 @@ function Testimonials() {
               ></span>
             </div>
           </div>
+          <p>Styling Still Under Processing ....</p>
         </SpaceY>
       </Container>
     </section>
