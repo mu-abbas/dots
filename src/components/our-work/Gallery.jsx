a5aeac6bd3eb0c0b641e8a0a02a06c44637684af
Muhammad Abbas <md.devjs@gmail.com>
2024-04-28 10:55:26 +0300

    add suspence to our-work

--- src/components/our-work/Gallery.jsx
+++ src/components/our-work/Gallery.jsx
@@ -5,6 +5,7 @@ import SpaceY from '../micro/SpaceY';
 import FilterButton from './FilterButton';
 import GalleryItem from './GalleryItem';
 import { useRouter } from 'next/navigation';
+import { Suspense } from 'react';
 
 const projects = [
   {
@@ -156,43 +157,45 @@ function Gallery() {
   }
 
   return (
-    <section>
-      <Container>
-        <SpaceY>
-          <header>
-            <div className="border-t-[0.5px] border-grey pt-8 opacity-25 lg:pt-10 2xl:pt-12 "></div>
-            <div className="flex flex-col flex-wrap items-center gap-2 sm:flex-row xl:gap-3 3xl:gap-4">
-              <FilterButton active={filter === 'all'} onClick={() => filterHandler('all')}>
-                All
-              </FilterButton>
-              <FilterButton
-                active={filter === 'presentation-design'}
-                onClick={() => filterHandler('presentation-design')}
-              >
-                Presentation Design
-              </FilterButton>
-              <FilterButton active={filter === 'pitch-deck'} onClick={() => filterHandler('pitch-deck')}>
-                Pitch Decks
-              </FilterButton>
-              <FilterButton
-                active={filter === 'presentation-template'}
-                onClick={() => filterHandler('presentation-template')}
-              >
-                Presentation Templates
-              </FilterButton>
-              <FilterButton active={filter === 'document-design'} onClick={() => filterHandler('document-design')}>
-                Document Design
-              </FilterButton>
+    <Suspense>
+      <section>
+        <Container>
+          <SpaceY>
+            <header>
+              <div className="border-t-[0.5px] border-grey pt-8 opacity-25 lg:pt-10 2xl:pt-12 "></div>
+              <div className="flex flex-col flex-wrap items-center gap-2 sm:flex-row xl:gap-3 3xl:gap-4">
+                <FilterButton active={filter === 'all'} onClick={() => filterHandler('all')}>
+                  All
+                </FilterButton>
+                <FilterButton
+                  active={filter === 'presentation-design'}
+                  onClick={() => filterHandler('presentation-design')}
+                >
+                  Presentation Design
+                </FilterButton>
+                <FilterButton active={filter === 'pitch-deck'} onClick={() => filterHandler('pitch-deck')}>
+                  Pitch Decks
+                </FilterButton>
+                <FilterButton
+                  active={filter === 'presentation-template'}
+                  onClick={() => filterHandler('presentation-template')}
+                >
+                  Presentation Templates
+                </FilterButton>
+                <FilterButton active={filter === 'document-design'} onClick={() => filterHandler('document-design')}>
+                  Document Design
+                </FilterButton>
+              </div>
+            </header>
+            <div className="grid grid-cols-1 gap-12 sm:gap-16 lg:grid-cols-2 lg:gap-12 2xl:gap-16 3xl:gap-20">
+              {filteredProjects.map(({ title, src, href }) => (
+                <GalleryItem src={src} title={title} href={href} key={Math.random()} />
+              ))}
             </div>
-          </header>
-          <div className="grid grid-cols-1 gap-12 sm:gap-16 lg:grid-cols-2 lg:gap-12 2xl:gap-16 3xl:gap-20">
-            {filteredProjects.map(({ title, src, href }) => (
-              <GalleryItem src={src} title={title} href={href} key={Math.random()} />
-            ))}
-          </div>
-        </SpaceY>
-      </Container>
-    </section>
+          </SpaceY>
+        </Container>
+      </section>
+    </Suspense>
   );
 }
 
