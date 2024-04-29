7486938cf4d2f826d78a2554c70b7c75f574f597
Muhammad Abbas <md.devjs@gmail.com>
2024-04-29 18:55:21 +0300

    start working on Packages section

--- src/components/retainership/Main.jsx
+++ src/components/retainership/Main.jsx
@@ -3,12 +3,14 @@ import Faqs from '../ui/Faqs';
 import ReadyToStart from '../ui/ReadyToStart';
 import Counter from './Counter';
 import Features from './Features';
+import Packages from './Packages';
 
 function Main() {
   return (
     <main className="bg-blue">
       <Features />
       <Counter />
+      <Packages />
       <Faqs />
       <ReadyToStart />
     </main>
---
+++ src/components/retainership/Packages.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Container from '../micro/Container';
+import SpaceY from '../micro/SpaceY';
+
+function Packages() {
+  return (
+    <section className="bg-offWhite">
+      <Container>
+        <SpaceY>
+          <p className="text-3xl text-center">Packages: TODO</p>
+        </SpaceY>
+      </Container>
+    </section>
+  );
+}
+
+export default Packages;
