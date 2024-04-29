bc54a268d3346333523d1dcb388756a86c67db59
Muhammad Abbas <md.devjs@gmail.com>
2024-04-29 12:46:53 +0300

    start working on section counter

---
+++ src/components/retainership/Counter.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import Container from '../micro/Container';
+import SpaceY from '../micro/SpaceY';
+
+function Counter() {
+  return (
+    <section className="text-center bg-black border-b text-offWhite border-offWhite">
+      <Container>
+        <SpaceY>Counter: TODO</SpaceY>
+      </Container>
+    </section>
+  );
+}
+
+export default Counter;
--- src/components/retainership/Main.jsx
+++ src/components/retainership/Main.jsx
@@ -1,12 +1,14 @@
 'use client';
 import Faqs from '../ui/Faqs';
 import ReadyToStart from '../ui/ReadyToStart';
+import Counter from './Counter';
 import Features from './Features';
 
 function Main() {
   return (
     <main className="bg-blue">
       <Features />
+      <Counter />
       <Faqs />
       <ReadyToStart />
     </main>
