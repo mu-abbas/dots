a8f7ed213d2a3c31d99d1b6cd3fc56a0aa4857d0
Muhammad Abbas <md.devjs@gmail.com>
2024-04-29 18:51:54 +0300

    end of section counter

--- src/components/retainership/Counter.jsx
+++ src/components/retainership/Counter.jsx
@@ -2,12 +2,21 @@
 
 import Container from '../micro/Container';
 import SpaceY from '../micro/SpaceY';
+import CounterItem from './CounterItem';
 
 function Counter() {
   return (
-    <section className="text-center bg-black border-b text-offWhite border-offWhite">
+    <section className="text-center bg-black text-offWhite">
       <Container>
-        <SpaceY>Counter: TODO</SpaceY>
+        <SpaceY>
+          <h2 className="text-2xl text-center md:text-3xl lg:text-4xl 3xl:text-5xl">So far we have accomplished</h2>
+          <div className="grid justify-center grid-cols-[15rem] md:grid-cols-[15rem_15rem] xl:grid-cols-[15rem_15rem_15rem_15rem] gap-6 3xl:grid-cols-[18rem_18rem_18rem_18rem] 3xl:gap-8">
+            <CounterItem number={300} sign="+" label="Clients" />
+            <CounterItem number={12} sign="+" label="Countries" />
+            <CounterItem number={95} sign="%" label="Long-term" />
+            <CounterItem number={100} sign="%" label="Satisfied" />
+          </div>
+        </SpaceY>
       </Container>
     </section>
   );
---
+++ src/components/retainership/CounterItem.jsx
@@ -0,0 +1,16 @@
+'use client';
+import CountUp from 'react-countup';
+
+function CounterItem({ number, sign, label }) {
+  return (
+    <figure className="flex flex-col items-center justify-center gap-4 bg-opacity-25 border border-opacity-15 rounded-3xl size-60 3xl:size-72 bg-grey border-lightGrey">
+      <div className="flex text-5xl text-blue 3xl:text-6xl">
+        <CountUp end={number} duration={5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={300} />
+        <span>{sign}</span>
+      </div>
+      <figcaption className="text-2xl font-light font-poppins 3xl:text-3xl">{label}</figcaption>
+    </figure>
+  );
+}
+
+export default CounterItem;
