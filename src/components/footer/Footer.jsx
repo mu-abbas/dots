0efa63f07061a9e16ff350c80bdf93a640207cc9
Muhammad Abbas <md.devjs@gmail.com>
2024-05-17 22:21:04 +0300

    start working on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
 'use client';
+
+import Container from '../micro/Container';
+
 function Footer() {
   return (
     <footer className="bg-black text-offWhite">
-      <div className="container p-6 mx-auto sm:p-8 md:p-10 lg:px-12 xl:px-14 2xl:px-16">
+      <Container>
         <p className="text-center">Footer: Under Processing...</p>
-      </div>
+      </Container>
     </footer>
   );
 }
